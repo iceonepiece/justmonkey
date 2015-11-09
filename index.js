0bf642105db99f76bc578f5a9b3ca375550aebf2
iceonepiece <iceonepiece@gmail.com>
2015-11-10 00:42:05 +0700

    add some files

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Just a litte monkey in the world');
+var express = require('express');
+var exphbs	= require('express-handlebars');
+
+var app = express();
+var PORT = 5000;
+
+app.engine('handlebars', exphbs( {defaultLayout: 'main'} ) );
+app.set('view engine', 'handlebars');
+
+app.get('/', function(req, res){
+	res.render('home');
+});
+
+app.listen( PORT, function() {
+  	console.log('Node app is running on port', PORT );
+});
