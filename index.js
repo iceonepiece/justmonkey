bc335c3eac2c3910b7af74d41d597ad436b2b4b0
iceonepiece <iceonepiece@gmail.com>
2015-11-14 09:10:54 +0700

    add node_modules

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var exphbs	= require('express-handlebars');
+var bodyParser = require('body-parser');
 
 var app = express();
 var PORT = 5000;
@@ -9,18 +10,30 @@ app.set('view engine', 'handlebars');
 
 app.use( express.static( __dirname + '/public' ) );
 
+// body-parser for post request
+app.use( bodyParser.urlencoded({ extended: true }) );
+app.use( bodyParser.json() );
+
 app.get('/',function(req,res){
 	res.render('index');
-})
+});
+
 app.get('/workflowCreation', function(req, res){
 	res.render('home');
 });
+
 app.get('/workflowExecution', function(req, res){
 	res.end('Workflow Execution');
 });
-app.get('/workflow',function(req,res){
 
+app.get('/workflow',function(req,res){
 	res.render('workflow');
+});
+
+app.post('/save_workflow', function(req, res){
+	console.log( req.body.xml );
+
+	res.end('YEAH');
 });
 
 app.listen( PORT, function() {
